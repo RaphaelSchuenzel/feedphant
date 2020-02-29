581ac05ef50746241881092b0eb850f178126aff
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-02-29 13:06:30 +0100

    add pug linting, update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,39 +5,42 @@ module.exports = {
         node: true
     },
     parserOptions: {
-        parser: 'babel-eslint'
+        parser: "babel-eslint"
     },
     extends: [
-        '@nuxtjs',
-        'plugin:nuxt/recommended'
+        "@nuxtjs",
+        "plugin:nuxt/recommended"
     ],
     // add your custom rules here
     rules: {
-        'nuxt/no-cjs-in-config': 'off',
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'indent': ['error', 4],
-        'no-trailing-spaces': ['error', {
-            'skipBlankLines': true,
-            'ignoreComments': true
+        "nuxt/no-cjs-in-config": "off",
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "indent": ["error", 4],
+        "no-trailing-spaces": ["error", {
+            "skipBlankLines": true,
+            "ignoreComments": true
         }],
-        'vue/script-indent': ['error', 4, { 'baseIndent': 1 }],
-        'vue/html-indent': ['error', 4, {
-            'alignAttributesVertically': false
+        "vue/script-indent": ["error", 4, { "baseIndent": 1 }],
+        "vue/html-indent": ["error", 4, {
+            "alignAttributesVertically": false
         }],
-        'vue/multiline-html-element-content-newline': ['error', {
-            'allowEmptyLines': true
+        "vue/multiline-html-element-content-newline": ["error", {
+            "allowEmptyLines": true
         }],
-        'vue/singleline-html-element-content-newline': 'off',
-        'no-tabs': 'off',
-        'semi': 'off',
-        'eol-last': 'off'
+        "vue/singleline-html-element-content-newline": "off",
+        "no-tabs": "off",
+        "semi": "off",
+        "eol-last": "off"
     },
-    'overrides': [
+    "plugins": [
+        "pug"
+    ],
+    "overrides": [
         {
-            'files': ['*.vue'],
-            'rules': {
-                'indent': 'off'
+            "files": ["*.vue"],
+            "rules": {
+                "indent": "off"
             }
         }
     ]  
