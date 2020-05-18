63c4620099523a57784b2e1bb1a733532d341b34
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-05-18 17:31:14 +0200

    update base sql

--- api/db/old/4_base.sql
+++ api/db/old/4_base.sql
@@ -4,7 +4,7 @@
 -- https://tableplus.com/
 --
 -- Database: local
--- Generation Time: 2020-05-18 17:28:47.1040
+-- Generation Time: 2020-05-15 21:21:07.5960
 -- -------------------------------------------------------------
 
 
@@ -12,7 +12,7 @@
 
 -- Table Definition
 CREATE TABLE "hubs"."board" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "name" varchar(255) NOT NULL,
     "title" varchar(255) NOT NULL,
     "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -24,7 +24,7 @@ CREATE TABLE "hubs"."board" (
 
 -- Table Definition
 CREATE TABLE "hubs"."board_post" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "board_id" uuid NOT NULL,
     "title" varchar NOT NULL,
     "content" varchar NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE "hubs"."board_post" (
 
 -- Table Definition
 CREATE TABLE "hubs"."board_post_comment" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "post_id" uuid NOT NULL,
     "owner" int4 NOT NULL,
     "content" varchar(2048) NOT NULL,
@@ -51,32 +51,31 @@ CREATE TABLE "hubs"."board_post_comment" (
 
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS board_post_comment_vote_id_seq;
+
 -- Table Definition
 CREATE TABLE "hubs"."board_post_comment_vote" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
-    "comment_id" uuid NOT NULL,
-    "owner" int4 NOT NULL,
-    "state" bool NOT NULL DEFAULT false,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    "id" int4 NOT NULL DEFAULT nextval('board_post_comment_vote_id_seq'::regclass),
+    PRIMARY KEY ("id")
+);
+
+-- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS board_post_state_id_seq;
+
+-- Table Definition
+CREATE TABLE "hubs"."board_post_state" (
+    "id" int4 NOT NULL DEFAULT nextval('board_post_state_id_seq'::regclass),
     PRIMARY KEY ("id")
 );
 
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
 -- Table Definition
-CREATE TABLE "hubs"."board_post_state" (
-    "id" varchar NOT NULL,
-    "name" varchar NOT NULL,
-    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
-);
-
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
--- Table Definition
 CREATE TABLE "hubs"."board_post_vote" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "post_id" uuid NOT NULL,
     "owner" int4 NOT NULL,
     "state" bool NOT NULL DEFAULT false,
@@ -89,7 +88,7 @@ CREATE TABLE "hubs"."board_post_vote" (
 
 -- Table Definition
 CREATE TABLE "hubs"."settings" (
-    "hub_id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "hub_id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "public" bool NOT NULL DEFAULT false,
     "title" varchar,
     "description" varchar,
@@ -120,7 +119,7 @@ CREATE TABLE "hubs"."settings_brand" (
 
 -- Table Definition
 CREATE TABLE "hubs"."subscription" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "stripe_customer_id" varchar,
     "stripe_subscription_id" varchar,
     "current_period_ends" timestamp,
@@ -133,7 +132,7 @@ CREATE TABLE "hubs"."subscription" (
 
 -- Table Definition
 CREATE TABLE "hubs"."user" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "email" varchar(256) NOT NULL DEFAULT NULL::character varying,
     "username" varchar(50) NOT NULL DEFAULT NULL::character varying,
     "avatar_url" varchar(4096) DEFAULT NULL::character varying,
@@ -146,7 +145,7 @@ CREATE TABLE "hubs"."user" (
 
 -- Table Definition
 CREATE TABLE "hubs"."user_auth" (
-    "id" uuid NOT NULL DEFAULT hubs.uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "user_id" uuid NOT NULL,
     "adapter" varchar NOT NULL DEFAULT NULL::character varying,
     "access_token" varchar NOT NULL DEFAULT NULL::character varying,
