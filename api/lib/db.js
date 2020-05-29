7ae41a12f27a380348d0729223ad68c9a37cc82c
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-05-29 18:52:25 +0200

    update db export

--- api/lib/db.js
+++ api/lib/db.js
@@ -2,42 +2,44 @@ const Sequelize = require('sequelize');
 const consola = require('consola');
 const config = require('../../config/config.json');
 
-module.exports = async (callback) => {
-    const sequelize = new Sequelize(config.api.db.database, config.api.db.user, config.api.db.password, {
-        host: config.api.db.host,
-        dialect: config.api.db.dialect,
-        port: config.api.db.port,
-        logging: config.api.debug ? consola.log : false,
-        define: {
-            // prevent sequelize from pluralizing table names
-            freezeTableName: true,
-            // make sequelize automatically create timestamp columns (created_at & updated_at)
-            timestamps: true,
-            // automatically convert camelCase table attributes to snake_case
-            underscored: true
-        }
-    });
+module.exports = ({
+    initializeDb: async (callback) => {
+        const sequelize = new Sequelize(config.api.db.database, config.api.db.user, config.api.db.password, {
+            host: config.api.db.host,
+            dialect: config.api.db.dialect,
+            port: config.api.db.port,
+            logging: config.api.debug ? consola.log : false,
+            define: {
+                // prevent sequelize from pluralizing table names
+                freezeTableName: true,
+                // make sequelize automatically create timestamp columns (created_at & updated_at)
+                timestamps: true,
+                // automatically convert camelCase table attributes to snake_case
+                underscored: true
+            }
+        });
 
-    // test connection
-    await sequelize.authenticate();
+        // test connection
+        await sequelize.authenticate();
 
-    consola.success('Sequelize: Connected to database ' + '\x1B[42m\x1B[30m%s\x1B[0m', config.api.db.database);
+        consola.success('Sequelize: Connected to database ' + '\x1B[42m\x1B[30m%s\x1B[0m', config.api.db.database);
 
-    // import sequelize models
-    const models = require('../models')({ sequelize });
+        // import sequelize models
+        const models = require('../models')({ sequelize });
 
-    // associate models where applicable
-    Object.keys(models).forEach((key) => {
-        if ('associate' in models[key]) {
-            models[key].associate(models);
-        }
-    });
+        // associate models where applicable
+        Object.keys(models).forEach((key) => {
+            if ('associate' in models[key]) {
+                models[key].associate(models);
+            }
+        });
 
-    // hard sync with database
-    await sequelize.sync({ force: true, schema: 'hub', logging: false });
+        // hard sync with database
+        await sequelize.sync({ force: true, schema: 'hub', logging: false });
 
-    consola.success('Sequelize: Successfully synced all models.');
+        consola.success('Sequelize: Successfully synced all models.');
 
-    // pass connection object in callback
-    callback(sequelize);
-}
+        // pass connection object in callback
+        callback(sequelize);
+    }
+})
