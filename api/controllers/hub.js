d1a999f4cb7f4241ba033a8e5e221caf02b966cd
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-02 19:33:15 +0200

    implement service layer

--- api/controllers/hub.js
+++ api/controllers/hub.js
@@ -1,25 +1,27 @@
 'use strict';
 
-import HubInterface from '../interfaces/hub'
+import HubService from '../services/hub'
 
 module.exports = ({ api, db, passport }) => ({
 
     // retrieve hub on requested subdomain (${url}.feedphant.com)
     getHub: async (req, res, next) => {
-        const hub = await HubInterface.get('1b20abec-4001-4479-92b9-03365cc06c3a');
+        const hub = await HubService.getHub(req.body);
 
         return res.json(hub);
     },
 
     // create a new hub on the requested subdomain (${url}.feedphant.com)
     createHub: async (req, res, next) => {
-        const hub = await HubInterface.create();
+        const hub = await HubService.createHub(req.body);
 
         return res.json(hub);
     },
 
     // update an existing hub on the requested subdomain (${url}.feedphant.com)
     updateHub: async (req, res, next) => {
-        return res.status(501).send('NOT IMPLEMENTED: PUT Hub');
+        const hub = await HubService.updateHub(req.body);
+
+        return res.json(hub);
     }
 });
---
+++ api/services/hub.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import HubInterface from '../interfaces/hub'
+
+module.exports = {
+    getHub: async (body) => {
+        const hub = await HubInterface.get('1b20abec-4001-4479-92b9-03365cc06c3a');
+
+        return hub;
+    },
+    createHub: async (body) => {
+        const hub = await HubInterface.create(details);
+
+        return hub;
+    },
+    updateHub: async (body) => {
+        return false;
+    }
+};
