f055c876afde69a89891b8de076beae724e33493
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-03 15:33:23 +0200

    wip interface query

--- api/services/hub.js
+++ api/services/hub.js
@@ -4,7 +4,7 @@ const HubInterface = require('../interfaces/hub');
 
 module.exports = {
     getHub: async (body) => {
-        const hub = await HubInterface.get('1b20abec-4001-4479-92b9-03365cc06c3a');
+        const hub = await HubInterface.get('1b20abec-4001-4479-92b9-03365cc06c3a', { tableName: 'setting' });
 
         return hub;
     },
