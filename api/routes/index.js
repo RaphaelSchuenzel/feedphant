0268b73b5f1e4b7a068d9a70b3ce5e5ba9cd98a9
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-03 15:19:36 +0200

    fix routes index

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,9 +2,9 @@
 
 const router = require('express').Router();
 
-module.exports = () => {
-    router.use('/hub', require('./hub'))
-    router.use('/boards', require('./boards'))
-    router.use('/auth', require('./auth'))
-    router.use('/users', require('./users'))
-}
+router.use('/hub', require('./hub'))
+router.use('/boards', require('./boards'))
+router.use('/auth', require('./auth'))
+router.use('/users', require('./users'))
+
+module.exports = router;
