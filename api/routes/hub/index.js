e3a8c6a61841e90b19add329eab1869b761e5f10
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-04 14:06:07 +0200

    basic request validation implementation

--- api/routes/hub/index.js
+++ api/routes/hub/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const hub = require('express').Router();
+const { celebrate, Joi, Segments } = require('celebrate');
 
 // controllers
 const HubController = require('../../controllers/hub');
 
 hub.route('/')
-    .get(HubController.getHub)
+    .get(celebrate({
+        [Segments.HEADERS]: Joi.object().keys({
+            host: Joi.string().uri().required()
+        })
+    }), HubController.getHub)
     .post(HubController.createHub)
     .put(HubController.updateHub);
 
