d03cd14d30f6e837d4f98e4a569ea2f01b006032
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-05 18:13:28 +0200

    update api index

--- api/index.js
+++ api/index.js
@@ -7,6 +7,10 @@ const routes = require('./routes');
 
 const app = express();
 
+if (config.api.debug) {
+    consola.warn('API is running with debugging tools activated. For use in production, make sure to disable the debug option within the API config.');
+}
+
 // lib
 const error = require('./lib/error');
 require('./lib/passport')({ app });
@@ -17,6 +21,10 @@ app.use(helmet());
 // logger
 app.use(morgan(config.api.debug ? 'dev' : 'tiny'));
 
+// internal middleware
+require('./middleware/cors')({ app, config });
+require('./middleware/bodyParser')({ app, config });
+
 // handle uncaught exceptions
 process.on('uncaughtException', (err) => {
     consola.error(err)
@@ -24,14 +32,6 @@ process.on('uncaughtException', (err) => {
     // mandatory (as per the Node docs)
     process.exit(1)
 });
-
-if (config.api.debug) {
-    consola.warn('API is running with debugging tools activated. For use in production, make sure to disable the debug option within the API config.');
-}
-
-// internal middleware
-require('./middleware/cors')({ app, config });
-require('./middleware/bodyParser')({ app, config });
 
 // api router
 app.use('/api', routes);
