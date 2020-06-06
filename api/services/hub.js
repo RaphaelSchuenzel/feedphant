cb225ffa8a7a3fafc930c0c8e1ea1475a22a154f
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-06 18:35:13 +0200

    update model references in hub service

--- api/services/hub.js
+++ api/services/hub.js
@@ -8,23 +8,23 @@ const UserInterface = require('../interfaces/user');
 module.exports = {
     createHub: async (body) => {
         const result = await sequelize.transaction(async (t) => {
-            const hub = await HubInterface.create(t, 'hub', null, {
+            const hub = await HubInterface.create(t, 'Hub', null, {
                 subdomain: body.subdomain
             });
 
             // create a record in hub brand model
-            await HubInterface.create(t, 'hubBrand', hub.dataValues.hubId, {
+            await HubInterface.create(t, 'HubBrand', hub.dataValues.hubId, {
                 name: body.productName
             });
 
             // create a record in user model
-            const user = await UserInterface.create(t, 'users', null, hub.dataValues.hubId, {
+            const user = await UserInterface.create(t, 'User', null, hub.dataValues.hubId, {
                 name: body.name,
                 email: body.email
             });
 
             // create a record in user auth model
-            const auth = await UserInterface.create(t, 'usersAuth', user.dataValues.userId, hub.dataValues.hubId, {
+            const auth = await UserInterface.create(t, 'UserAuth', user.dataValues.userId, hub.dataValues.hubId, {
                 adapter: body.adapter,
                 hash: body.password,
                 accessToken: body.accessToken,
