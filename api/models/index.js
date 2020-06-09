bd4b2584c97d4dd25fb691ababa832dd2bc2f847
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-09 18:42:20 +0200

    update model naming structure

--- api/models/index.js
+++ api/models/index.js
@@ -8,14 +8,14 @@ const schema = config.api.db.schema;
 module.exports = ({ sequelize }) => {
     const models = {
         Board: require('./board')({ sequelize, Sequelize, schema }),
-        BoardPost: require('./board/post')({ sequelize, Sequelize, schema }),
-        BoardPostState: require('./board/post/state')({ sequelize, Sequelize, schema }),
-        BoardPostVote: require('./board/post/vote')({ sequelize, Sequelize, schema }),
-        BoardPostComment: require('./board/post/comment')({ sequelize, Sequelize, schema }),
-        BoardPostCommentVote: require('./board/post/comment/vote')({ sequelize, Sequelize, schema }),
         Hub: require('./hub')({ sequelize, Sequelize, schema }),
         HubBrand: require('./hub/brand')({ sequelize, Sequelize, schema }),
-        HubSubscription: require('./hub/subscription')({ sequelize, Sequelize, schema }),
+        Subscription: require('./subscription')({ sequelize, Sequelize, schema }),
+        Post: require('./post')({ sequelize, Sequelize, schema }),
+        PostState: require('./post/state')({ sequelize, Sequelize, schema }),
+        PostVote: require('./post/vote')({ sequelize, Sequelize, schema }),
+        PostComment: require('./post/comment')({ sequelize, Sequelize, schema }),
+        PostCommentVote: require('./post/comment/vote')({ sequelize, Sequelize, schema }),
         User: require('./user')({ sequelize, Sequelize, schema }),
         UserAuth: require('./user/auth')({ sequelize, Sequelize, schema })
     }
