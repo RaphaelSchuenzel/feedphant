0431a608ad8ddb985e083c09ff01227185f2813f
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-19 18:25:07 +0200

    fix hub file name + simplify models export

--- api/models/index.js
+++ api/models/index.js
@@ -5,20 +5,16 @@ const config = require('../../config/config.json');
 
 const schema = config.api.db.schema;
 
-module.exports = ({ sequelize }) => {
-    const models = {
-        Board: require('./board')({ sequelize, Sequelize, schema }),
-        BoardPost: require('./board/post')({ sequelize, Sequelize, schema }),
-        BoardPostState: require('./board/post/state')({ sequelize, Sequelize, schema }),
-        BoardPostVote: require('./board/post/vote')({ sequelize, Sequelize, schema }),
-        BoardPostComment: require('./board/post/comment')({ sequelize, Sequelize, schema }),
-        BoardPostCommentVote: require('./board/post/comment/vote')({ sequelize, Sequelize, schema }),
-        Brand: require('./brand')({ sequelize, Sequelize, schema }),
-        Hub: require('./hub')({ sequelize, Sequelize, schema }),
-        Subscription: require('./board/subscription')({ sequelize, Sequelize, schema }),
-        User: require('./user')({ sequelize, Sequelize, schema }),
-        UserAuth: require('./user/auth')({ sequelize, Sequelize, schema })
-    }
-
-    return models;
-}
+module.exports = ({ sequelize }) => ({
+    Board: require('./board')({ sequelize, Sequelize, schema }),
+    BoardPost: require('./board/post')({ sequelize, Sequelize, schema }),
+    BoardPostState: require('./board/post/state')({ sequelize, Sequelize, schema }),
+    BoardPostVote: require('./board/post/vote')({ sequelize, Sequelize, schema }),
+    BoardPostComment: require('./board/post/comment')({ sequelize, Sequelize, schema }),
+    BoardPostCommentVote: require('./board/post/comment/vote')({ sequelize, Sequelize, schema }),
+    Brand: require('./brand')({ sequelize, Sequelize, schema }),
+    Hub: require('./hub')({ sequelize, Sequelize, schema }),
+    Subscription: require('./board/subscription')({ sequelize, Sequelize, schema }),
+    User: require('./user')({ sequelize, Sequelize, schema }),
+    UserAuth: require('./user/auth')({ sequelize, Sequelize, schema })
+});
