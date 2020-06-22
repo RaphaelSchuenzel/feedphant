94f11bc4cf58835c7ad7fe4180b66acf7eb3413e
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-22 15:56:41 +0200

    add logger

--- api/middleware/render.js
+++ api/middleware/render.js
@@ -1,9 +1,13 @@
+const logger = require('../lib/logger');
+
 module.exports = (controller) => {
     return async (req, res, next) => {
         try {
             return res.json(await controller(req))
         } catch (err) {
             // todo: handle error
+            logger.error(err);
+
             return res.status(err.status || 500).json(err || {});
         }
     }
