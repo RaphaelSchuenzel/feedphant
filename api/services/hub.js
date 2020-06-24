02bbe4c2d4da48f577ea69dc72d9dfe471635e0a
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-24 18:28:05 +0200

    update service exports

--- api/services/hub.js
+++ api/services/hub.js
@@ -2,81 +2,81 @@
 
 const { sequelize, queryInterface } = require('../lib/db');
 
-module.exports = {
-    createHub: async ({ hub, user }) => {
-        const result = await sequelize.transaction(async (t) => {
-            // create a record in hub model
-            const hubQuery = await queryInterface.create({
-                transaction: t,
-                model: 'Hub',
-                params: {
-                    subdomain: hub.subdomain
-                }
-            });
-
-            // create a record in hub brand model
-            await queryInterface.create({
-                transaction: t,
-                model: 'HubBrand',
-                foreignKeys: {
-                    hubId: hubQuery.dataValues.id
-                },
-                params: {
-                    name: hub.brand.productName
-                }
-            });
-
-            // create a record in user model
-            const userQuery = await queryInterface.create({
-                transaction: t,
-                model: 'HubUser',
-                foreignKeys: {
-                    hubId: hubQuery.dataValues.id
-                },
-                params: {
-                    name: user.name,
-                    email: user.email
-                }
-            });
-
-            // create a record in auth model
-            const authQuery = await queryInterface.create({
-                transaction: t,
-                model: 'HubUserAuth',
-                foreignKeys: {
-                    userId: userQuery.dataValues.id
-                },
-                params: {
-                    adapter: user.auth.adapter,
-                    hash: user.auth.password,
-                    accessToken: user.auth.accessToken,
-                    refreshToken: user.auth.refreshToken
-                }
-            });
-
-            // return the access token of the newly created user
-            return authQuery.dataValues.accessToken;
+exports.createHub = async ({ hub, user }) => {
+    const result = await sequelize.transaction(async (t) => {
+        // create a record in hub model
+        const hubQuery = await queryInterface.create({
+            transaction: t,
+            model: 'Hub',
+            params: {
+                subdomain: hub.subdomain
+            }
         });
 
-        return result;
-    },
-    getHub: async ({ id, subdomain }) => {
-        const result = await sequelize.transaction(async (t) => {
-            const hubQuery = await queryInterface.get({
-                transaction: t,
-                model: 'Hub',
-                identifiers: {
-                    id,
-                    subdomain
-                }
-            });
-
-            return hubQuery;
+        // create a record in hub brand model
+        await queryInterface.create({
+            transaction: t,
+            model: 'HubBrand',
+            foreignKeys: {
+                hubId: hubQuery.dataValues.id
+            },
+            params: {
+                name: hub.brand.productName
+            }
         });
 
-        return result;
-    },
-    updateHub: (body) => {
-        return false;
-    }
+        // create a record in user model
+        const userQuery = await queryInterface.create({
+            transaction: t,
+            model: 'HubUser',
+            foreignKeys: {
+                hubId: hubQuery.dataValues.id
+            },
+            params: {
+                name: user.name,
+                email: user.email
+            }
+        });
+
+        // create a record in auth model
+        const authQuery = await queryInterface.create({
+            transaction: t,
+            model: 'HubUserAuth',
+            foreignKeys: {
+                userId: userQuery.dataValues.id
+            },
+            params: {
+                adapter: user.auth.adapter,
+                hash: user.auth.password,
+                accessToken: user.auth.accessToken,
+                refreshToken: user.auth.refreshToken
+            }
+        });
+
+        // return the access token of the newly created user
+        return authQuery.dataValues.accessToken;
+    });
+
+    return result;
 };
+
+exports.getHub = async ({ id, subdomain }) => {
+    const result = await sequelize.transaction(async (t) => {
+        const hubQuery = await queryInterface.get({
+            transaction: t,
+            model: 'Hub',
+            identifiers: {
+                id,
+                subdomain
+            }
+        });
+
+        return hubQuery;
+    });
+
+    return result;
+};
+
+exports.updateHub = (body) => {
+    return false;
+}
--- api/services/users.js
+++ api/services/users.js
@@ -1,33 +1,31 @@
 'use strict';
 
-module.exports = {
-    getUser: (condition, value) => {
-        return new Promise((resolve, reject) => {
-            if (condition && value) {
-                // allow lookup for unique conditions only
+exports.getUser = (condition, value) => {
+    return new Promise((resolve, reject) => {
+        if (condition && value) {
+            // allow lookup for unique conditions only
 
-                if (condition === 'id' || condition === 'email') {
-                    /* db.query(`SELECT * FROM users WHERE ${condition} = '${value}'`, function (error, results, fields) {
-                        if (error) {
-                            reject(new ApplicationError(ErrorCodes.INTERNAL_ERROR, error));
-                        } else if (results) {
-                            const response = {};
-                            
-                            response.id = results[0].id;
-                            response.username = results[0].username;
-                            response.avatar = results[0].avatar_url;
-                            
-                            return resolve(response);
-                        } else {
-                            return resolve({});
-                        }
-                    }); */
-                } else {
-                    reject(new ApplicationError(ErrorCodes.INTERNAL_ERROR, 'Invalid condition.'));
-                }
+            if (condition === 'id' || condition === 'email') {
+                /* db.query(`SELECT * FROM users WHERE ${condition} = '${value}'`, function (error, results, fields) {
+                    if (error) {
+                        reject(new ApplicationError(ErrorCodes.INTERNAL_ERROR, error));
+                    } else if (results) {
+                        const response = {};
+                        
+                        response.id = results[0].id;
+                        response.username = results[0].username;
+                        response.avatar = results[0].avatar_url;
+                        
+                        return resolve(response);
+                    } else {
+                        return resolve({});
+                    }
+                }); */
             } else {
-                reject(new ApplicationError(ErrorCodes.INTERNAL_ERROR, 'Missing arguments.'));
+                reject(new ApplicationError(ErrorCodes.INTERNAL_ERROR, 'Invalid condition.'));
             }
-        });
-    }
-};
+        } else {
+            reject(new ApplicationError(ErrorCodes.INTERNAL_ERROR, 'Missing arguments.'));
+        }
+    });
+}
