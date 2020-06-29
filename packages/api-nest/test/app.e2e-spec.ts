33e875d9c2c0f24b03cfa1a4b1fb5afebabe798e
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2020-06-29 12:03:02 +0200

    update nest api file indentation

--- packages/api-nest/test/app.e2e-spec.ts
+++ packages/api-nest/test/app.e2e-spec.ts
@@ -6,19 +6,20 @@ import { INestApplication } from '@nestjs/common';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeAll(async () => {
+        const moduleFixture = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+        await app.init();
+    });
+
+    it('/ (GET)', () => {
+        return request(app.getHttpServer())
+            .get('/')
+            .expect(200)
+            .expect('Hello World!');
+    });
 });
